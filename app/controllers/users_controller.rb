9aa1eba52f56aed2b960a07d1ae01040cd155f0c
Kevin Lord <lordkev@gmail.com>
2011-01-10 15:29:43 -0600

    Cleaned up user signup and flash display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
+      @user.password.clear
+      @user.password_confirmation.clear
       @title = "Sign up"
       render 'new'
     end
